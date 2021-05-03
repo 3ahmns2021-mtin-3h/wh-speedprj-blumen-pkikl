de2baebeb996a9d1ae598ea0068ea589ffe4648d
pkikl <peter.kikl02@htl-salzburg.ac.at>
2021-05-03 14:16:04 +0200

    After Lesson Progress

---
+++ Assets/MyGame/Scripts/Blumen.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blumen : MonoBehaviour
+{
+    private int spacePress;
+    private int kPress;
+
+    public float startTime, stopTime, timer;
+    bool isTimerRunning;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spacePress = 0;
+        kPress = 0;
+        isTimerRunning = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = stopTime + (Time.time - startTime);
+        Debug.Log("Overall " + timer);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("hello spacy" + spacePress);
+            spacePress++;
+        }
+        
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            TimerStart();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            //Debug.Log("hello k" + kPress);
+            kPress++;
+        }
+    }
+    void TimerStart()
+    {
+        if(isTimerRunning)
+        {
+            startTime = Time.time;
+        }
+        
+    }
+    void TimerStop()
+    {
+        if(isTimerRunning)
+        {
+            stopTime = Time.time;
+        }
+
+    }
+    void TimerReset()
+    {
+        timer = startTime = stopTime = 0.0f;
+    }
+}
