cc303185007f6aba9d2ba18755964350c35af62e
pkikl <peter.kikl02@htl-salzburg.ac.at>
2021-05-10 13:24:13 +0200

    process in lesson

--- Assets/MyGame/Scripts/Blumen.cs
+++ Assets/MyGame/Scripts/Blumen.cs
@@ -6,10 +6,13 @@ public class Blumen : MonoBehaviour
 {
     private int spacePress;
     private int kPress;
+    private bool isSpacePressed;
+    private bool isTimerRunning;
+    private bool isKPressed;
 
     public float startTime, stopTime, timer;
-    bool isTimerRunning;
-
+    public GameObject prefabBlume;
+    public GameObject parentObj;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +20,42 @@ public class Blumen : MonoBehaviour
         spacePress = 0;
         kPress = 0;
         isTimerRunning = false;
+        isSpacePressed = false;
+        isKPressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         timer = stopTime + (Time.time - startTime);
-        Debug.Log("Overall " + timer);
+        //Debug.Log("Overall " + timer);
+
+        int seconds = (int)timer % 60;
+
+        if (isTimerRunning)
+        {
+            if(isSpacePressed && seconds >= 1)
+            {
+          
+                isSpacePressed = false;
+                TimerStop();
+                TimerReset();
+                CreateBlumen(spacePress);
+            }
+            if(isKPressed && seconds >= 1)
+            {
+                isKPressed = false;
+                TimerStop();
+                TimerReset();
+            }
+        }
+
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
             //Debug.Log("hello spacy" + spacePress);
             spacePress++;
+            isSpacePressed = true;
         }
         
         if(Input.GetKeyUp(KeyCode.Space))
@@ -44,9 +71,10 @@ public class Blumen : MonoBehaviour
     }
     void TimerStart()
     {
-        if(isTimerRunning)
+        if(isTimerRunning == false)
         {
             startTime = Time.time;
+            isTimerRunning = true;
         }
         
     }
@@ -55,11 +83,24 @@ public class Blumen : MonoBehaviour
         if(isTimerRunning)
         {
             stopTime = Time.time;
+            isTimerRunning = false;
         }
 
     }
     void TimerReset()
     {
         timer = startTime = stopTime = 0.0f;
+        //Debug.Log("TimerReset");
+    }
+
+    public void CreateBlumen(int numberBlumen)
+    {
+        Debug.Log("create Blumen");
+
+        for (int i = 0; i < numberBlumen; i++)
+        {
+
+            Instantiate(prefabBlume, new Vector2(i * 2.0f, 0),Quaternion.identity, parentObj.transform);
+        }
     }
 }
